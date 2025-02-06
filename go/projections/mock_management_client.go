43a13150354bb0cc79e48ed2c281c01c8e5e42ac
Jesko Plitt <jp@becklyn.com>
2025-02-06 18:18:43 +0100

    Fix projections management client mock

--- go/projections/mock_management_client.go
+++ go/projections/mock_management_client.go
@@ -10,37 +10,30 @@ type MockManagementClient struct {
 	mock.Mock
 }
 
-func (c *MockManagementClient) RegisterMigration(
+func (c *MockManagementClient) DeploySchema(
 	ctx context.Context,
 	namespace string,
-	dangerouslyRemoveGdprFields bool,
 	projectionTypes []ObjectType,
 	crudTypes []ObjectType,
 	nestedTypes []ObjectType,
 	enums []EnumType,
 	views []View,
+	options *DeploymentOptions,
 ) error {
-	return c.Called(ctx, namespace, dangerouslyRemoveGdprFields, projectionTypes, crudTypes, nestedTypes, enums, views).Error(0)
+	return c.Called(ctx, namespace, projectionTypes, crudTypes, nestedTypes, enums, views, options).Error(0)
 }
 
-func (c *MockManagementClient) ApplyMigration(ctx context.Context, namespace string) error {
-	return c.Called(ctx, namespace).Error(0)
+func (c *MockManagementClient) ConfirmSchema(ctx context.Context, deploymentId int64) error {
+	return c.Called(ctx, deploymentId).Error(0)
 }
 
-func (c *MockManagementClient) CleanupMigration(ctx context.Context, namespace string, status map[string]int64) error {
-	return c.Called(ctx, namespace, status).Error(0)
+func (c *MockManagementClient) RollbackSchema(ctx context.Context, deploymentId int64) error {
+	return c.Called(ctx, deploymentId).Error(0)
 }
 
-func (c *MockManagementClient) RollbackMigration(ctx context.Context, namespace string) error {
-	return c.Called(ctx, namespace).Error(0)
-}
-
-func (c *MockManagementClient) GetMigrationStatus(ctx context.Context, namespace string) (*MigrationStatus, error) {
-	args := c.Called(ctx, namespace)
-	if args.Get(0) == nil {
-		return nil, args.Error(1)
-	}
-	return args.Get(0).(*MigrationStatus), args.Error(1)
+func (c *MockManagementClient) GetSchemaDeployment(ctx context.Context, deploymentId int64) (uint32, error) {
+	args := c.Called(ctx, deploymentId)
+	return args.Get(0).(uint32), args.Error(1)
 }
 
 func (c *MockManagementClient) Close() error {
