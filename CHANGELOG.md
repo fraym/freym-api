3ccea232da697d35409b2c7193d9ab4273538efe
Jesko Plitt <jp@becklyn.com>
2025-11-12 16:45:26 +0100

    Better logging in streams connection

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # vNext
 
+# v0.50.1
+
+- (improvement) Better logging in streams connection
+
 # v0.50.0
 
 - (feature) Add `parallelTopicProcessing` option to streams client
--- go/streams/internal/subscription/connection.go
+++ go/streams/internal/subscription/connection.go
@@ -103,6 +103,7 @@ loop:
 
 		go func() {
 			if err := handlerFn(response); err != nil {
+				c.logger.Debug().WithError(err).Write("handler function returned an error")
 				errChan <- err
 				return
 			}
